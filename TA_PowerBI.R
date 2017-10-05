8b041478bd3261cb5566111ddf13bb38244f886e
grousell <grousell@gmail.com>
2017-10-04 20:55:59 -0400

    Recode Grade for sorting

--- TA_PowerBI.R
+++ TA_PowerBI.R
@@ -47,12 +47,20 @@ sem2 <- read_excel("C:/Users/grousell/OneDrive - Grand Erie DSB/Report Card/Data
 SixNation_Sem1 <- df %>%
   filter (month == 10) %>%
   left_join(sem1, by = c("StudentID" = "OEN")) %>%
-  mutate (AllCredits = ifelse (Percent > 99, 1, 0))
+  mutate (AllCredits = ifelse (Percent > 99, 1, 0)) %>%
+  mutate (Grade_Sort = recode(GradeCode, 
+                              "9" = "01_9",
+                              "10" = "02_10",
+                              "11" = "03_11",
+                              "12" = "04_12"))
 
 SixNation_Sem2 <- df %>%
   filter (month == 3) %>%
   left_join(sem2, by = c("StudentID" = "OEN")) %>%
   mutate (AllCredits = ifelse (Percent > 99, 1, 0)) %>%
-  mutate (TEST = ifelse (Percent <50, "Less than 50", "Greater than 50"))
-
+  mutate (Grade_Sort = recode(GradeCode, 
+                              "9" = "01_9",
+                              "10" = "02_10",
+                              "11" = "03_11",
+                              "12" = "04_12"))
 
